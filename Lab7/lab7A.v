0ab259dba32b0c8cc5b59876d6347ef2fe26a474
Joel Guo <jguo48@gmail.com>
2016-11-28 20:43:07 -0600

    copied testbench from textbook

--- Lab7/lab7A.v
+++ Lab7/lab7A.v
@@ -1,34 +1,58 @@
 // You can use this skeleton testbench code, the textbook testbench code, or your own
 module MIPS_Testbench ();
   reg CLK;
-  reg RST;
-  wire CS;
-  wire WE;
-  wire [31:0] Mem_Bus;
-  wire [6:0] Address;
+  wire CS, WE;
 
-  initial
-  begin
-    CLK = 0;
-  end
+  parameter N = 10;
+  reg[31:0] expected[N:1];
+
+  wire[31:0] Mem_Bus;
+  reg[31:0] AddressTB;
+  wire[6:0] Address, Address_Mux;
+  wire WE_Mux, CS_Mux;
+  reg init, RST, WE_TB, CS_TB;
+
+  integer i;
 
   MIPS CPU(CLK, RST, CS, WE, Address, Mem_Bus);
   Memory MEM(CS, WE, CLK, Address, Mem_Bus);
 
-  always
-  begin
-    #5 CLK = !CLK;
-  end
+  assign Address_Mux = (init) ? AddressTB : Address;
+  assign WE_Mux = (init) ? WE_TB : WE;
+  assign CS_Mux = (init) ? CS_TB;
 
-  always
-  begin
-    RST <= 1'b1; //reset the processor
+  always #5 CLK = !CLK;
 
+  initial begin
+    expected[1] = 32'h00000006;
+    expected[2] = 32'h00000012;
+    expected[3] = 32'h00000018;
+    expected[4] = 32'h0000000C;
+    expected[5] = 32'h00000002;
+    expected[6] = 32'h00000016;
+    expected[7] = 32'h00000006;
+    expected[8] = 32'h00000001;
+    expected[9] = 32'h00000120;
+    expected[10] = 32'h00412022;
+    CLK = 0;
+  end //initial
+
+  always begin
     //Notice that the memory is initialize in the in the memory module not here
-
+    RST <= 1'b1; //reset the processor
+    @(posedge CLK);
+    init <= 1; CS_TB <= 1; WE_TB <= 1;
+    @(posedge CLK);
+    CS_TB <= 0; WE_TB <= 0; init <= 0;
     @(posedge CLK);
     // driving reset low here puts processor in normal operating mode
     RST = 1'b0;
+    for (i = 1; i <= N; i = i+1) begin
+      @(posedge WE);
+      @(negedge CLK);
+      if (Mem_Bus != expected[i])
+        $display("Output mismatch: got %d, expect %d", Mem_Bus, expected[i]);
+    end//for
 
     /* add your testing code here */
     // you can add in a 'Halt' signal here as well to test Halt operation
@@ -37,7 +61,7 @@ module MIPS_Testbench ();
 
     $display("TEST COMPLETE");
     $stop;
-  end
+  end//always
 
 endmodule
 
